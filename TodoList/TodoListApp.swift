ce4d4f608d7f54422a6feedae06ba8d37e1ccd8c
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2021-12-17 17:52:51 -0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Pavan Mikkilineni on 17/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
